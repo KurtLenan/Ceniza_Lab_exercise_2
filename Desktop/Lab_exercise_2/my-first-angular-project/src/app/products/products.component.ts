df988fd4fc69e60c551e40a34d9bb93e360d686c
Kurt Lenan Ceniza <ceniza.kurtlenan2005@gmail.com>
2026-02-23 13:17:56 +0800

    Lab 3-Dynamic products with angular-updated

--- Desktop/Lab_exercise_2/my-first-angular-project/src/app/products/products.component.ts
+++ Desktop/Lab_exercise_2/my-first-angular-project/src/app/products/products.component.ts
@@ -1,45 +1,76 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent {
 
+  cartCount: number = 0;
+  totalPrice: number = 0;
+  selectedCategory: string = 'All';
+  showCategories: boolean = true;  
+
   products = [
-      {
-        name: 'Laptop',
-        price: 45000,
-        available: true,
-        image: 'images/thunder-robot.jpg'
-      },
-      {
-        name: 'Phone',
-        price: 15000,
-        available: false,
-        image: 'images/tecno-camon-40.jpg'
-      },
-      {
-        name: 'Headphones',
-        price: 2000,
-        available: true,
-        image: 'images/wireless-headphone.jpg'
-      },
-      {
-        name: 'Keyboard',
-        price: 1200,
-        available: true,
-        image: 'images/keyboard.jpg'
-      }
-    ];
+    {
+      name: 'ThunderRobot',
+      price: 45000,
+      available: true,
+      image: 'images/thunder-robot.jpg',
+      category: 'Devices'
+    },
+    {
+      name: 'TecnoCamon40',
+      price: 15000,
+      available: false,
+      image: 'images/tecno-camon-40.jpg',
+      category: 'Devices'
+    },
+    {
+      name: 'Headphone',
+      price: 2000,
+      available: true,
+      image: 'images/wireless-headphone.jpg',
+      category: 'Accessories'
+    },
+    {
+      name: 'Keyboard',
+      price: 1200,
+      available: true,
+      image: 'images/keyboard.jpg',
+      category: 'Accessories'
+    }
+  ];
+
   addToCart(product: any) {
-   if (product.available) {
-    alert(product.name + ' added to cart!');
+    if (product.available) {
+      this.cartCount++;
+      this.totalPrice += product.price;
+      alert(product.name + ' added to cart!');
+    }
   }
-}
+
+  toggleCategories() {
+    this.showCategories = !this.showCategories;
+  }
+
+  setCategory(category: string) {
+    this.selectedCategory = category;
+  }
+
+  get filteredProducts() {
+    if (this.selectedCategory === 'All') {
+      return this.products;
+    }
+
+    return this.products.filter(
+      product => product.category === this.selectedCategory
+    );
+  }
 
 }
